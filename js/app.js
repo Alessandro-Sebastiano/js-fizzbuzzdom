a2ee1209f4fbd9e565c6ddb4407f67ca7782371d
Alessandro-Sebastiano <alesebastiano15@gmail.com>
2022-10-11 10:35:42 +0200

    print-box

--- js/app.js
+++ js/app.js
@@ -1,20 +1,43 @@
 "use strict";
 
+var container = document.querySelector('.container');
 
 
 
 for (var i = 1; i <= 100; i++) {
 
     if (i % 3 != 0 && i % 5 != 0) {
-        console.log(i);
+        makeBox(i);
+        //console.log(i);
     }
 
     if (i % 3 == 0 && i % 5 == 0) {
-        console.log('FizzBuzz');
+        makeBox('FizzBuzz');
+        //console.log('FizzBuzz');
     } else if (i % 3 == 0) {
-        console.log('Fizz');
+        //console.log('Fizz');
+        makeBox('Fizz');
     } else if (i % 5 == 0) {
-        console.log('Buzz');
+        makeBox('Buzz');
+        //console.log('Buzz');
+    }
+
+}
+
+
+function makeBox(element) {
+
+    const box = document.createElement('div');
+    box.classList.add('box');
+    container.append(box);
+    box.innerHTML = element;
+
+    if (element == 'Fizz') {
+        box.style.backgroundColor = 'green';
+    } else if (element == 'Buzz') {
+        box.style.backgroundColor = 'yellow';
+    } else if (element == 'FizzBuzz') {
+        box.style.backgroundColor = 'lightblue';
     }
 
 }
